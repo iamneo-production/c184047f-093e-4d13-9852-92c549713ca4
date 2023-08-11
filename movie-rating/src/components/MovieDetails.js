1dc07dbbf6935a1c17909694f5ca6e544e62522a
MSaiSudha <mondisaisudha@gmail.com>
2023-08-11 05:02:17 +0000

    MovieDetails

--- movie-rating/src/components/MovieDetails.js
+++ movie-rating/src/components/MovieDetails.js
@@ -1,5 +1,6 @@
 import "./MovieDetails.css";
 import {useState} from "react";
+import StarRating from "./StarRating.js"
 const movies=[
     { id: 1,
       title: "Inception",
@@ -7,16 +8,16 @@ const movies=[
       poster:
       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
       synopsis: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
-      rating: 8.8,
+      rating: 4,
       reviews: [
         {
-          rating: 9.0,
+          rating: 4,
           review: "A complex movie",
           user: "balajimurali",
           email: "balajimurali1999@gmail.com"
         },
         {
-            rating: 8.8,
+            rating: 4,
             review: "Thriller",
             user: "tammayya",
             email:"tammayyaarava789@gmail.com"
@@ -27,10 +28,10 @@ const movies=[
         title: "The Shawshank Redemption",
         year: 1994,
         synopsis: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-        rating: 9.3,
+        rating: 5,
         reviews: [
         {
-          rating: 9.0,
+          rating: 5,
           review: "A good movie",
           user: "balajimurali",
           email: "balajimurali1999@gmail.com"
@@ -41,13 +42,20 @@ const movies=[
         title: "The Dark Knight",
         year: 2008,
         synopsis: "When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.",
-        rating: 9.0,
+        rating: 5,
         reviews: []
       }
     ]
 export default function MovieDetails(){
     const [isOpen,setOpen]=useState(true);
+    const [userRating, setUserRating] = useState("");
+    const [userReview,setUserReview]=useState("");
     const movie=movies.at(0);
+    function submitReview(){
+        console.log(userRating+" "+userReview);
+        setUserRating("");
+        setUserReview("");
+    }
     return (
         <main>
             <button className="toggle" onClick={()=>setOpen(!isOpen)}> Go To Back </button>
@@ -55,7 +63,19 @@ export default function MovieDetails(){
             <br/><br/>
             <table className="#table">
                 <tr>
-                    <td colspan="2"><img src={movie.poster} alt={`${movie.title} poster`} height="200" width="200" /></td>
+                    <td>
+                        <img src={movie.poster} alt={`${movie.title} poster`} height="200" width="200" />
+                    </td>
+                    <td>
+                        <p><i>You're feedback !</i>
+                        <div className="rating">
+                        <StarRating maxRating={5} size={24} onSetRating={setUserRating}/>
+                        <input className="review" type="text" placeholder="write your review" value={userReview}
+                            onChange={(e)=>setUserReview(e.target.value)}/>
+                        </div>
+                        <button onClick={submitReview}>submit</button>
+                        </p> 
+                    </td>
                 </tr>
                 <tr>
                     <td><h3>Movie Name</h3></td>
