aca0d47c9d13bade23a4517d22d65366a493626b
pavan-code <pavankumaranguluri1@gmail.com>
2023-08-11 08:12:10 +0000

    updated login page

--- movie-rating/src/App.js
+++ movie-rating/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+<<<<<<< Updated upstream
 import Loginpage from './components/login/Login';
 function App() {
   return (
     <div className="App">
        Movies thammayya appp
        <Loginpage />
+=======
+import Login from './components/login/Login';
+
+function App() {
+  return (
+    <div className="App">
+       <Login />
+>>>>>>> Stashed changes
     </div>
     
   );
